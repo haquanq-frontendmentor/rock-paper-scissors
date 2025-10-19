5f65203214fdad48d033294426c26fc887656b9d
Ha Quanq <quangha0120010@gmail.com>
2025-10-19 22:54:55 +0700

    Config base path for vite build

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,5 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [svgr(), react(), tailwindcss()],
+    base: "/rock-paper-scissors",
 });
